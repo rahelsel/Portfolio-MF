713320b883282165ecde4ee7a83c802c3c29b9b5
Rae Helsel <rahelsel@me.com>
2016-09-28 21:04:49 -0700

    finished template and fixed append issue

--- js/render_sources.js
+++ js/render_sources.js
@@ -11,7 +11,11 @@ function Schedule(schedule) {
 
 Schedule.prototype.toHtml = function() {
   var $scheduleTemplate = $('.template').clone();
-  $scheduleTemplate.find('h2').text(this.studioLocation);
+  $scheduleTemplate.find('h2').text(this.studioName);
+  $scheduleTemplate.find('#location').text(this.studioLocation);
+  $scheduleTemplate.find('#className').text(this.className);
+  $scheduleTemplate.find('#classDate').text(this.classDate);
+  $scheduleTemplate.find('#classTime').text(this.classTime);
   $scheduleTemplate.removeClass('template');
   $scheduleTemplate.addClass('schedule');
   return $scheduleTemplate;
